f42db0b4e00f77a164ccc88c718500fb9f86bdbb
hvardhan <harsh.vardhan@greenwavesystems.com>
2018-05-02 11:05:43 +0530

    Recipe View Changes

--- HelloFresh/app/src/main/java/hellofresh/test/com/hellofresh/ui/list/RecipeListViewModel.java
+++ HelloFresh/app/src/main/java/hellofresh/test/com/hellofresh/ui/list/RecipeListViewModel.java
@@ -18,10 +18,8 @@ package hellofresh.test.com.hellofresh.ui.list;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
-import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.Transformations;
 import android.arch.lifecycle.ViewModel;
-import android.support.annotation.Nullable;
 import android.support.annotation.VisibleForTesting;
 
 import java.util.List;
@@ -30,7 +28,6 @@ import javax.inject.Inject;
 
 import hellofresh.test.com.hellofresh.repository.RecipeRepository;
 import hellofresh.test.com.hellofresh.util.AbsentLiveData;
-import hellofresh.test.com.hellofresh.util.Objects;
 import hellofresh.test.com.hellofresh.vo.Recipe;
 import hellofresh.test.com.hellofresh.vo.Resource;
 
@@ -40,11 +37,8 @@ public class RecipeListViewModel extends ViewModel {
 
     private final LiveData<Resource<List<Recipe>>> recipes;
 
-    private final NextPageHandler nextPageHandler;
-
     @Inject
     public RecipeListViewModel(RecipeRepository recipeRepository) {
-        nextPageHandler = new NextPageHandler(recipeRepository);
         recipes = Transformations.switchMap(query, search -> {
             if (search == null || search.trim().length() == 0) {
                 return AbsentLiveData.create();
@@ -60,22 +54,7 @@ public class RecipeListViewModel extends ViewModel {
     }
 
     public void setQuery() {
-        nextPageHandler.reset();
         query.setValue("1");
-    }
-
-    @VisibleForTesting
-    public LiveData<LoadMoreState> getLoadMoreStatus() {
-        return nextPageHandler.getLoadMoreState();
-    }
-
-    @VisibleForTesting
-    public void loadNextPage() {
-        String value = query.getValue();
-        if (value == null || value.trim().length() == 0) {
-            return;
-        }
-        nextPageHandler.queryNextPage(value);
     }
 
     public void refresh() {
@@ -84,101 +63,5 @@ public class RecipeListViewModel extends ViewModel {
         }
         else
             query.setValue("");
-    }
-    static class LoadMoreState {
-        private final boolean running;
-        private final String errorMessage;
-        private boolean handledError = false;
-
-        LoadMoreState(boolean running, String errorMessage) {
-            this.running = running;
-            this.errorMessage = errorMessage;
-        }
-
-        boolean isRunning() {
-            return running;
-        }
-
-        String getErrorMessage() {
-            return errorMessage;
-        }
-
-        String getErrorMessageIfNotHandled() {
-            if (handledError) {
-                return null;
-            }
-            handledError = true;
-            return errorMessage;
-        }
-    }
-
-    @VisibleForTesting
-    static class NextPageHandler implements Observer<Resource<Boolean>> {
-        @Nullable
-        private LiveData<Resource<Boolean>> nextPageLiveData;
-        private final MutableLiveData<LoadMoreState> loadMoreState = new MutableLiveData<>();
-        private String query;
-        private final RecipeRepository repository;
-        @VisibleForTesting
-        boolean hasMore;
-
-        @VisibleForTesting
-        NextPageHandler(RecipeRepository repository) {
-            this.repository = repository;
-            reset();
-        }
-
-        void queryNextPage(String query) {
-            if (Objects.equals(this.query, query)) {
-                return;
-            }
-            unregister();
-            this.query = query;
-            //nextPageLiveData = repository.searchNextPage(query);
-            loadMoreState.setValue(new LoadMoreState(true, null));
-            //noinspection ConstantConditions
-            nextPageLiveData.observeForever(this);
-        }
-
-        @Override
-        public void onChanged(@Nullable Resource<Boolean> result) {
-            if (result == null) {
-                reset();
-            } else {
-                switch (result.status) {
-                    case SUCCESS:
-                        hasMore = Boolean.TRUE.equals(result.data);
-                        unregister();
-                        loadMoreState.setValue(new LoadMoreState(false, null));
-                        break;
-                    case ERROR:
-                        hasMore = true;
-                        unregister();
-                        loadMoreState.setValue(new LoadMoreState(false,
-                                result.message));
-                        break;
-                }
-            }
-        }
-
-        private void unregister() {
-            if (nextPageLiveData != null) {
-                nextPageLiveData.removeObserver(this);
-                nextPageLiveData = null;
-                if (hasMore) {
-                    query = null;
-                }
-            }
-        }
-
-        private void reset() {
-            unregister();
-            hasMore = true;
-            loadMoreState.setValue(new LoadMoreState(false, null));
-        }
-
-        MutableLiveData<LoadMoreState> getLoadMoreState() {
-            return loadMoreState;
-        }
     }
 }
